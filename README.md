cfb516da35e15f9e070787e6a4bea40412ffaae6
ElizabethEmilia <listencpp@gmail.com>
2021-02-22 03:39:55 +0800

    add scripts

--- README.md
+++ README.md
@@ -10,25 +10,21 @@
 <img src="docs/preview3.png">
 
 ## 如何使用？
-* Project setup
-```
-npm install
-```
+**首先，编译Web项目**
 
-* Compiles and hot-reloads for development
-```
-npm run serve
-```
+0. 安装依赖： `npm install`
+1. 构建项目： `npm run build`，生成的代码在dist目录下。复制所有dist目录下的文件到nginx的web root根目录。
 
-* Compiles and minifies for production
-```
-npm run build
-```
+**然后生成图片缓存以及相关json文件**
 
-* Lints and fixes files
-```
-npm run lint
-```
+0. 安装依赖：`pip3 install numpy pillow`
+1. 在scripts目录下，新建一个albums目录，这个目录用来存放相册。
+2. 在albums目录下新建目录，每一个目录都是一个相册，建议使用英语名，例如：`cutecy`。新建一个和之前的目录名同名的.txt文件可以自定义一个好看的名称，例如新建`cutecy.txt`，并在该文件中保存文本`超越妹妹可可爱爱`，会在相册的网页界面中显示后面TXT文本的内容。在每个相册的目录都可以放入任意的图片。
+3. **运行 generate-jsons.py。**
+4. **在nginx的web root根目录下新建一个api目录，并将albums、album-caches以及生成的所有json文件放入该目录。**
 
-* Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+注意：之后每一次更新完照片以后，都需要重新执行上述3-4步骤。
+
+---------
+
+by Miyuki, 2020.1, Licensed under MIT license
---
+++ scripts/generate-jsons.py
@@ -0,0 +1,148 @@
+import numpy as np
+import os
+from PIL import Image
+import json
+import pathlib
+import math
+
+def get_album_path(album_name=''):
+    if album_name != '':
+        album_name = album_name + '/'
+    return './album/{}'.format(album_name)
+
+def get_album_thumbnail_path(album_name=''):
+    if album_name != '':
+        album_name = album_name + '/'
+    return './album-cache/{}'.format(album_name)
+
+def is_album_path_existed(album_name=''):
+    return os.path.isdir(get_album_path(album_name))
+
+def is_album_cache_path_existed(album_name=''):
+    return os.path.isdir(get_album_thumbnail_path(album_name))
+
+def create_cache_dir(album_name=''):
+    if is_album_cache_path_existed(album_name):
+        return
+    os.mkdir(get_album_thumbnail_path(album_name))
+
+def write_json_file(path, obj):
+    with open(path, 'w', encoding='utf8') as file:
+        file.write(json.dumps(obj))
+
+def read_file(path):
+    try:
+        with open(path, 'r', encoding='utf8') as file:
+            return file.read()
+    except:
+        return None
+
+# 获取相册列表,并创建缓存目录
+def get_album_list():
+    if not is_album_path_existed():
+        raise Exception('Album directory {} does not exist'.format(get_album_path()))
+    create_cache_dir()
+    files = os.listdir(get_album_path())
+    album_names = []
+    for f in files:
+        if os.path.isdir(get_album_path(f)):
+            album_names.append(f)
+            create_cache_dir(f)
+    return album_names
+
+
+## 生成 /api/get-album.json
+def generate_json__get_album():
+    def get_metadata_filename(album_name):
+        return '{}{}.txt'.format(get_album_path(), album_name)
+    album_names = get_album_list()
+    jsonobj = []
+
+    for aname in album_names:
+        preview_name = ""
+        # get preview
+        files = os.listdir(get_album_path(aname))
+        if len(files) > 0:
+            preview_name = files[0]
+        metafile = get_metadata_filename(aname)
+        if os.path.isfile(metafile):
+            jsonobj.append({"name": aname, "friendly_name": read_file(metafile), "preview": preview_name})
+        else:
+            jsonobj.append({"name": aname, "friendly_name": aname, "preview": preview_name})
+    write_json_file('get-album.json', jsonobj)
+    return album_names
+
+## 生成图片的缩略图并返回图片信息
+def make_thumbnail(album_name, image_file_name):
+    cf = '{}{}'.format(get_album_thumbnail_path(album_name), image_file_name)
+    im = Image.open('{}{}'.format(get_album_path(album_name), image_file_name))
+    width, height = im.size
+    if (os.path.isfile(cf)):
+        im.close()
+        return width, height
+    dst_width = 256
+    dst_height = int(dst_width*height/width)
+    im.thumbnail((dst_width, dst_height))
+    print('Generating thumbnail for: {}/{}'.format(album_name, image_file_name))
+    im.save(cf)
+    im.close()
+    return width, height
+
+def generate_photo_by_page(image_data, filename_callback):
+    photo_for_each_page = 50
+    page_count = math.ceil(len(image_data) / 50)
+    for page in range(page_count):
+        ph_obj = []
+        for i in range(page*photo_for_each_page, min(len(image_data), (page+1)*photo_for_each_page)):
+            ph_obj.append(image_data[i])
+        #write_json_file("{}-get-photo-page-{}.json".format(album_name, page), ph_obj)
+        filename = filename_callback(page)
+        write_json_file(filename, ph_obj)
+
+# 相片
+    ##  /api/{album_name}-get-photo-count.json  相片数量
+    ##  /api/{album_name}-get-photo-page-{num}.json  生成缩略图并获取每一页的相片列表
+    ##  /api/get-photo-count.json  相片数量
+    ##  /api/get-photo-page-{num}.json  生成缩略图并获取每一页的相片列表
+    ##  /api/get-recent-photo-count.json  相片数量
+    ##  /api/get-recent-photo-page-{num}.json  生成缩略图并获取每一页的相片列表
+def generate_json_album_related(album_name=''):
+    if album_name == '':
+        album_names = get_album_list()
+        image_objs = []
+        for aname in album_names:
+            image_objs = image_objs + generate_json_album_related(aname)
+        write_json_file("get-photo-count.json", { "count": len(image_objs) })
+        generate_photo_by_page(image_objs, lambda page: "get-photo-page-{}.json".format(page))
+        ## 最近
+        write_json_file("get-recent-photo-count.json", {"count": len(image_objs)})
+        image_objs.sort(key=lambda e: e["ct"], reverse=True)
+        generate_photo_by_page(image_objs, lambda page: "get-recent-photo-page-{}.json".format(page))
+        return
+    # 保存每一个相册的照片信息
+    photos = os.listdir(get_album_path(album_name))
+    image_data = []
+    for pf in photos:
+        pfl = pf.lower()
+        if os.path.isfile('{}{}'.format(get_album_path(album_name), pf)) and (pfl[-4:] in ['.png', '.jpg', '.gif'] or pfl[-5:] in ['.jpeg', '.tiff']):
+            ## 生成缩略图
+            width, height = make_thumbnail(album_name, pf)
+            fd = pathlib.Path('{}{}'.format(get_album_path(album_name), pf))
+            ctime = fd.stat().st_ctime
+            image_data.append({ "al": album_name, "name": pf, "h": height, "w": width, "ct": ctime })
+    # 生成相片数量
+    count_obj = { "count": len(image_data) }
+    write_json_file("{}-get-photo-count.json".format(album_name), count_obj)
+    generate_photo_by_page(image_data, lambda page: "{}-get-photo-page-{}.json".format(album_name, page))
+
+    return image_data
+
+if __name__ == '__main__':
+    # 生成相册列表  /api/get-album.json
+    generate_json__get_album()
+
+    # 相片
+    ##  /api/{album_name}-get-photo-count.json  相片数量
+    ##  /api/{album_name}-get-photo-page-{num}.json  生成缩略图并获取每一页的相片列表
+    generate_json_album_related()
+
